588c0eece6b9fe44017ff070cf9b7232fe9dd58d
viralbthakar <viralbthakar@gmail.com>
2024-10-30 21:23:48 -0400

    AWS Utils Added - Upload and Download from S3

---
+++ utils_python_aws/utils.py
@@ -0,0 +1,38 @@
+import re
+import boto3
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def extract_bucket_key_from_s3_uri(s3_uri):
+    """
+    Extracts the bucket name and key from an S3 URI.
+    Args:
+        s3_uri (str): The S3 URI in the format 's3://bucket_name/key'.
+    Returns:
+        tuple: A tuple containing the bucket name and key as strings.
+    """
+    s3_pattern = re.compile(r"^s3://([^/]+)/(.*)$")
+    match = s3_pattern.match(s3_uri)
+    bucket_name = match.group(1)
+    key = match.group(2)
+    return bucket_name, key
+
+
+def upload_file_to_s3(file_path, s3_uri):
+    """
+    Uploads a file to an S3 bucket.
+    Args:
+        file_path (str): The local path of the file to upload.
+        s3_uri (str): The S3 URI of the file to upload.
+    Returns:
+        bool: True if the file was uploaded successfully, False otherwise.
+    """
+    bucket_name, key = extract_bucket_key_from_s3_uri(s3_uri)
+    s3 = boto3.client("s3")
+    try:
+        s3.upload_file(file_path, bucket_name, key)
+        return True
+    except Exception as e:
+        logger.error(f"Error uploading file to S3: {s3_uri}: {e}")
